f434c2d2127965626a2d4bc26efbdf0adaaca886
lizujun <lizejun@lingxing.com>
2022-07-22 09:02:19 +0800

    update: 更新版本

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,16 @@
 </template>
 
 <script lang="ts">
-  import {
-    defineComponent
-  } from 'vue'
+import { defineComponent } from "vue";
 
-  export default defineComponent({
-    name: 'App',
-
-  })
+export default defineComponent({
+  name: "App",
+});
 </script>
 
 <style>
-  #app {
-    width: 100%;
-    height: 100%;
-  }
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/types/routerType.ts
+++ src/types/routerType.ts
@@ -1,27 +1,26 @@
 /*
- * @Description: 
+ * @Description:
  */
-const { Interface } = require("node:readline");
-import Home from '../components/Home.vue'
+import Home from "../components/Home.vue";
 
 /*
- * @Description: 
+ * @Description:
  */
 interface IRouter {
-  id?: number | string
-  name: string
-  path: string
-  redirect?: string | {name: string}
+  id?: number | string;
+  name: string;
+  path: string;
+  redirect?: string | { name: string };
   meta?: {
-      icon: string
-      title: string
-      hidden?: boolean
-      permission?: Array<string>
-  }
-  hidden?: boolean
+    icon: string;
+    title: string;
+    hidden?: boolean;
+    permission?: Array<string>;
+  };
+  hidden?: boolean;
 }
 
 export interface IRouterList extends IRouter {
-  component: (() => Promise<typeof import('*.vue')>) | typeof Home
-  children?: Array<IRouterList>
+  component: (() => Promise<typeof import("*.vue")>) | typeof Home;
+  children?: Array<IRouterList>;
 }
